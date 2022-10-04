632e16ec3aa355d34138265f26bfdcc4ac2cca7e
ezhang6811 <eric.zhang6811@gmail.com>
2022-10-04 00:18:24 -0700

    added socials to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa"
 import React from "react";
 
 export default function Navbar() {
@@ -7,18 +8,18 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+            Eric Zhang
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+          <a href="https://www.linkedin.com/in/ericzhang68/" className="mr-5 hover:text-white">
+            <FaLinkedin />
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Skills
+          <a href="https://github.com/ezhang6811" className="mr-5 hover:text-white">
+            <FaGithub />
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
+          <a href="https://www.instagram.com/ezzhanggg/" className="mr-5 hover:text-white">
+            <FaInstagram />
           </a>
         </nav>
         <a
